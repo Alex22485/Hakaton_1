08b0def96761fb9d1049beabf18151f8a61a0776
Vova <tarabanvv@mail.ru>
2023-11-04 11:40:43 +0800

    ОПЯТЬ СУКА НЕ ДАЕТ РАБОТАТЬ БЕЗ КОММИТА Я ЕБАЛ

---
+++ src/modules/shape/shape.module.js
@@ -0,0 +1,21 @@
+import { Module } from '../../core/module';
+import { tasks, random } from '../../utils';
+import './shape.css';
+export class ShapeModule extends Module {
+	constructor(type, text) {
+		super(type, text);
+	}
+	trigger(textContent) {
+		if (textContent === tasks[1]) {
+			let screenWidth = window.innerWidth;
+			let screenHeight = window.innerHeight;
+			const body = document.querySelector('body');
+			const shape = document.createElement('div');
+			shape.classList = `shape${random(0, 50)}`;
+			shape.position = 'absolute';
+			shape.style.top = `${random(100, screenHeight - 100)}px`;
+			shape.style.left = `${random(100, screenWidth - 100)}px`;
+			body.append(shape);
+		}
+	}
+}
