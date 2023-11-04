fd615be385144770ba274fb2f6274fba6a5573a6
Alex22485 <alex.224.85@mail.ru>
2023-11-04 11:46:57 +0700

    сделал случайный фон в  background.module.js

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,14 @@ export function random(min, max) {
 }
 
 export const tasks =['Аналитика кликов', 'Случайная фигура', 'Таймер отсчета', 'Случайный звук', 'Случайный фон', 'Кастомное сообщение', 'Собственный модуль']
+
+// рандомный цвет она
+export const getRandonColor = () =>{
+    let elem = 'RGB('
+    for(let i = 1; i<4; i++){
+      elem += `${255 - Math.round(Math.random()*100)},`
+  }
+  let randomElem = elem.slice(0,-1) + ")"
+
+  return randomElem
+}
