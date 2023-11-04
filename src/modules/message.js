d5e16670dad72457f5173c3a0a5edd9ae4b196bd
Vova <tarabanvv@mail.ru>
2023-11-04 23:58:59 +0800

    message block and debug shape

--- src/modules/message.js
+++ src/modules/message.js
@@ -1,12 +1,25 @@
-import {Module} from '../core/module'
-import {tasks} from '../utils'
+import { Module } from '../core/module';
+import { tasks, MESSAGES } from '../utils';
+import { random } from '../utils';
+import { contextMenu } from '../app';
+
 export class Message extends Module {
-    constructor(type, text){
-        super(type, text)
-    }
-    trigger(textContent) {
-        if (textContent === tasks[5]){
-            console.log(tasks[5]);
-        }
-      }
+	constructor(type, text) {
+		super(type, text);
+	}
+	trigger(textContent) {
+		if (textContent === tasks[5]) {
+			let screenWidth = window.innerWidth;
+			let screenHeight = window.innerHeight;
+			const body = document.querySelector('body');
+			const divMessage = document.createElement('div');
+			divMessage.style.position = 'absolute';
+			divMessage.style.top = `${random(100, screenHeight - 100)}px`;
+			divMessage.style.left = `${random(100, screenWidth - 100)}px`;
+			divMessage.textContent = MESSAGES[random(0, MESSAGES.length - 1)];
+			body.append(divMessage);
+
+			contextMenu.close();
+		}
+	}
 }
--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,47 @@
 export function random(min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+	return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
 
-export const tasks =['Аналитика кликов', 'Случайная фигура', 'Таймер отсчета', 'Случайный звук', 'Случайный фон', 'Кастомное сообщение', 'Собственный модуль']
+export const tasks = [
+	'Аналитика кликов',
+	'Случайная фигура',
+	'Таймер отсчета',
+	'Случайный звук',
+	'Случайный фон',
+	'Кастомное сообщение',
+	'Собственный модуль'
+];
 
 // рандомный цвет она
-export const getRandonColor = () =>{
-    let elem = 'RGB('
-    for(let i = 1; i<4; i++){
-      elem += `${255 - Math.round(Math.random()*100)},`
-  }
-  let randomElem = elem.slice(0,-1) + ")"
+export const getRandonColor = () => {
+	let elem = 'RGB(';
+	for (let i = 1; i < 4; i++) {
+		elem += `${255 - Math.round(Math.random() * 100)},`;
+	}
+	let randomElem = elem.slice(0, -1) + ')';
 
-  return randomElem
-}
+	return randomElem;
+};
+
+export const MESSAGES = [
+	'Привет!',
+	'Hello!',
+	'Как дела?',
+	'How are you?',
+	'Что нового?',
+	"What's new?",
+	'Хорошего дня!',
+	'Have a nice day!',
+	'Добро пожаловать!',
+	'Welcome!',
+	'Как прошел день?',
+	'How was your day?',
+	'Удачи!',
+	'Good luck!',
+	'Какой у тебя план на сегодня?',
+	"What's your plan for today?",
+	'Спасибо!',
+	'Thank you!',
+	'Приятно познакомиться!',
+	'Nice to meet you!'
+];
