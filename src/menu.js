1f8ab3392c3bab0884fa2b804826c72d074aa561
Samat Malkvaev <102961337+FixelMiks@users.noreply.github.com>
2023-11-05 19:34:17 +0600

    Revert "Developer create shape and message function"

--- src/menu.js
+++ src/menu.js
@@ -1,36 +1,5 @@
-import { Menu } from './core/menu';
-import { tasks } from './utils';
+import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
-	constructor(el) {
-		super(el);
-		this.start = true
-	}
 
-	open() {
-		document.addEventListener('contextmenu', event => {
-			event.preventDefault();
-			
-			// блокировка открытия меню если модуль не
-			if(this.start){
-				this.el.textContent = '';
-				this.el.style.position = 'fixed';
-				this.el.style.left = event.clientX + 'px';
-				this.el.style.top = event.clientY + 'px';
-	
-				tasks.forEach(task => {
-					const li = document.createElement('li');
-					li.classList = 'menu-item';
-					li.textContent = task;
-					this.el.append(li);
-				});
-				this.el.style.display = 'inline';
-			}
-
-		});
-	}
-	close() {
-		this.el.style.display = 'none';
-	}
-	add() {}
 }
